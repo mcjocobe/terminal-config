d757979986b3423c24f123a34c7a256dc7ad227a
Pablo Escodebar <escodebar@gmail.com>
2024-04-01 23:00:09 +0200

    Configure zsh using home manager

--- nix-darwin/home-manager/escodebar.nix
+++ nix-darwin/home-manager/escodebar.nix
@@ -4,5 +4,29 @@
     nodenv
   ];
   home.stateVersion = "24.05";
+  programs.fzf.enable = true;
   programs.home-manager.enable = true;
+  programs.zsh = {
+    dotDir = ".config/zsh";
+    enable = true;
+    enableCompletion = true;
+    history.ignoreAllDups = true;
+    historySubstringSearch.enable = true;
+    initExtra = ''
+      [[ ! -f ${./p10k.zsh} ]] || source ${./p10k.zsh}
+    '';
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
+    ];
+    syntaxHighlighting.enable = true;
+  };
 }
