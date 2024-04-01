88af3932de619726fe6090624b1e1ae209fdf157
Pablo Escodebar <escodebar@gmail.com>
2024-04-01 13:12:28 +0200

    Move vim powerline setup to vim plugin

--- nix-darwin/overlays.nix
+++ nix-darwin/overlays.nix
@@ -5,6 +5,11 @@
 }: let
   vim-overlay = final: prev: let
     python = prev.pkgs.python311.withPackages (ps: with ps; [powerline]);
+    vim-powerline = prev.pkgs.vimUtils.buildVimPlugin {
+      name = "powerline";
+      namePrefix = "vim-";
+      src = "${prev.pkgs.python311Packages.powerline}/share/vim";
+    };
     vim-configured =
       (prev.pkgs.vim-full.override {
         python3 = python;
@@ -23,6 +28,7 @@
                 syntastic
                 vim-colors-solarized
                 vim-fugitive
+                vim-powerline
                 vim-sensible
               ];
             };
