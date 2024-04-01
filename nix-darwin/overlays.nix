c96a1b0a914687e60c4e0780e73088c157902062
Pablo Escodebar <escodebar@gmail.com>
2024-04-01 13:06:29 +0200

    Move vim plugin management to overlay

--- nix-darwin/overlays.nix
+++ nix-darwin/overlays.nix
@@ -1,10 +1,34 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   vim-overlay = final: prev: let
     python = prev.pkgs.python311.withPackages (ps: with ps; [powerline]);
-    vim-configured = prev.pkgs.vim-full.override {
-      python3 = python;
-      wrapPythonDrv = true;
-    };
+    vim-configured =
+      (prev.pkgs.vim-full.override {
+        python3 = python;
+        wrapPythonDrv = true;
+      })
+      .customize {
+        name = "vim";
+        vimrcConfig = {
+          customRC = lib.readFile ./configs/vimrc;
+          packages = {
+            vim-configured = with prev.pkgs;
+            with vimPlugins; {
+              start = [
+                editorconfig-vim
+                fzf-vim
+                syntastic
+                vim-colors-solarized
+                vim-fugitive
+                vim-sensible
+              ];
+            };
+          };
+        };
+      };
   in {
     inherit vim-configured;
   };
