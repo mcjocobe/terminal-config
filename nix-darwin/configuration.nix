dcdde448e0a7c8a7aec969b4d3f861ab479a866a
Pablo Escodebar <escodebar@gmail.com>
2024-04-07 07:05:24 +0200

    Configure vim as the system's editor

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -4,6 +4,9 @@
   ...
 }: let
 in {
+  environment.variables = {
+    EDITOR = "vim";
+  };
   nix.settings.experimental-features = "nix-command flakes";
   nixpkgs.hostPlatform = "aarch64-darwin";
   programs.zsh.enable = true;
